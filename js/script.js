f24492ab164357651f276bf3d33cefe664df4ec9
Franpa99 <franparietti@gmail.com>
2023-09-03 01:12:40 -0300

    Funcion nueva

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ function ageCalculator() {
     const dob = new Date("1999-11-05");
     const currentDate = new Date();
     /* Calcula la diferencia en años entre la fecha de nacimiento y la fecha actual */
-    const age = currentDate.getFullYear() - dob.getFullYear();
+    let age = currentDate.getFullYear() - dob.getFullYear();
     /* Verifica si el cumpleaños ya ha ocurrido este año */
     if (currentDate.getMonth() < dob.getMonth() || (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
         age--;
     }
     /* Muestra la edad en el encabezado */
     const header = document.querySelector('header');
-    header.innerHTML += `<p>Tengo ${age} años y soy un apasionado del desarrollo web y de videojuegos, oriundo de Montevideo, Uruguay.</p>`;
+    header.innerHTML = `<h1>Francisco Parietti Morixe</h1>
+                        <p>¡Hola! Soy Francisco, un apasionado del desarrollo web, desarrollo de videojuegos y fútbol de ${age} años, oriundo de Montevideo, Uruguay.</p>`;
 }
 
 /* Llama a la función al cargar la página para mostrar la edad en el encabezado */
